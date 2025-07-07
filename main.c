7f14e8d5f3939afdcb3b19a4305be4ed1c7584b0
mvghasty <fasmagoric.dev@gmail.com>
2025-07-06 23:10:40 -0300

    Add eyes to the cube and eyes interaction with mouse

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main(void) {
     const int screenWidth = 800;
@@ -52,13 +53,63 @@ int main(void) {
             DrawText("THE CUBE COOL GAME!", 10, 10, 20, LIGHTGRAY);
             DrawRectangleRec(floor, GRAY);
             DrawRectangleRec(player, WHITE);
+
+            Vector2 mousePos = GetMousePosition();
+
+            float eyeWidth = 12.0f;
+            float eyeHeight = 10.0f;
+            float pupilWidth = 5.0f;
+            float pupilHeight = 5.0f;
+            float maxOffset = 3.0f;
+
+            Vector2 leftEyeCenter = { player.x + 10, player.y + 12 };
+            Vector2 rightEyeCenter = { player.x + 30, player.y + 12 };
+
+            Vector2 leftDir = Vector2Subtract(mousePos, leftEyeCenter);
+            Vector2 rightDir = Vector2Subtract(mousePos, rightEyeCenter);
+
+            leftDir = Vector2Normalize(leftDir);
+            rightDir = Vector2Normalize(rightDir);
+
+            Vector2 leftPupil = {
+                leftEyeCenter.x + leftDir.x * maxOffset,
+                leftEyeCenter.y + leftDir.y * maxOffset
+            };
+
+            Vector2 rightPupil = {
+                rightEyeCenter.x + rightDir.x * maxOffset,
+                rightEyeCenter.y + rightDir.y * maxOffset
+            };
+
+            DrawRectangleRec((Rectangle){
+                leftEyeCenter.x - eyeWidth / 2,
+                leftEyeCenter.y - eyeHeight / 2,
+                eyeWidth, eyeHeight
+            }, BLACK);
+
+            DrawRectangleRec((Rectangle){
+                rightEyeCenter.x - eyeWidth / 2,
+                rightEyeCenter.y - eyeHeight / 2,
+                eyeWidth, eyeHeight
+            }, BLACK);
+
+            DrawRectangleRec((Rectangle){
+                leftPupil.x - pupilWidth / 2,
+                leftPupil.y - pupilHeight / 2,
+                pupilWidth, pupilHeight
+            }, RAYWHITE);
+
+            DrawRectangleRec((Rectangle){
+                rightPupil.x - pupilWidth / 2,
+                rightPupil.y - pupilHeight / 2,
+                pupilWidth, pupilHeight
+            }, RAYWHITE);
         EndDrawing();
     }
 
     UnloadSound(fxJump);
-
     CloseAudioDevice();
-
     CloseWindow();
+
     return 0;
 }
