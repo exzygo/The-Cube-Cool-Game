3e4b6f36478e14ed2c905e4c459de5370aa952f5
mvghasty <fasmagoric.dev@gmail.com>
2025-07-08 15:24:08 -0300

    Code refactoring, add ternary operators

--- main.c
+++ main.c
@@ -12,26 +12,26 @@
 extern int input_key(unsigned char *keys);
 
 int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screen_width = 800;
+    const int screen_height = 450;
     const float gravity = 0.5f;
-    const float jumpForce = -10.0f;
+    const float jump_force = -10.0f;
 
-    InitWindow(screenWidth, screenHeight, "THE CUBE COOL GAME");
+    InitWindow(screen_width, screen_height, "THE CUBE COOL GAME");
     InitAudioDevice();
     SetTargetFPS(60);
 
     Rectangle player = { 100, 300, P_X, P_Y };
-    Rectangle floor = { 0, 400, screenWidth, 50 };
+    Rectangle floor = { 0, 400, screen_width, 50 };
 
-    Sound fxJump = LoadSound("assets/sounds/jump.wav");
+    Sound fx_jump = LoadSound("assets/sounds/jump.wav");
     
     Cursor cursor;
     Vector2 hotspot = {CURSOR_X / 2, CURSOR_Y / 2}; 
     LoadCursor(&cursor, "assets/cursors/c1.png", hotspot);
 
-    float velocityY = 0.0f;
-    int isOnGround = 1;
+    float velocity_y = 0.0f;
+    int is_on_ground = 1;
 
     while (!WindowShouldClose()) {
         unsigned char keys[5];
@@ -43,31 +43,31 @@ int main(void) {
 
         int key = input_key(keys);
 
-        velocityY += gravity;
-        player.y += velocityY;
+        velocity_y += gravity;
+        player.y += velocity_y;
 
-        if (GetScreenWidth() != screenWidth || GetScreenHeight() != screenHeight) SetWindowSize(screenWidth, screenHeight);
+        if (GetScreenWidth() != screen_width || GetScreenHeight() != screen_height) SetWindowSize(screen_width, screen_height);
 
         if (player.y + player.height >= floor.y) {
             player.y = floor.y - player.height;
-            velocityY = 0;
-            isOnGround = 0;
+            velocity_y = 0;
+            is_on_ground = 0;
         } else {
-            isOnGround = 1;
+            is_on_ground = 1;
         }
 
-        if (player.x < 0) player.x = 0;
-        if (player.x + player.width > screenWidth) player.x = screenWidth - player.width;
+        (player.x < 0) ? player.x = 0 : 0;
+        (player.x + player.width > screen_width) ? player.x = screen_width - player.width : 0;
         if (player.y < 0) {
             player.x = 0;
-            velocityY = 0;
+            velocity_y = 0;
         }
 
-        if (key == KEY_RIGHT || key == KEY_D) player.x += SPEED;
-        if (key == KEY_LEFT || key == KEY_A) player.x -= SPEED;
-        if (IsKeyDown(KEY_SPACE) && isOnGround == 0) {
-            velocityY = jumpForce;
-            PlaySound(fxJump);
+        player.x += (key == KEY_RIGHT || key == KEY_D) ? SPEED : 0;
+        player.x -= (key == KEY_LEFT || key == KEY_A) ? SPEED : 0;
+        if (IsKeyDown(KEY_SPACE) && is_on_ground == 0) {
+            velocity_y = jump_force;
+            PlaySound(fx_jump);
         }
 
         BeginDrawing();
@@ -82,7 +82,7 @@ int main(void) {
         EndDrawing();
     }
 
-    UnloadSound(fxJump);
+    UnloadSound(fx_jump);
     UnloadCursor(&cursor);
 
     CloseAudioDevice();
